8fc20cb7bc2b86e917e16c7dcf544ac07b1ed3ef
Bit-Dev <bitdev.contacto@gmail.com>
2016-02-10 11:38:29 +0100

    Añadir script

---
+++ language.js
@@ -0,0 +1,30 @@
+function initTranslation(sectionID){
+  var languageID = 'es';
+  if(languageID && sectionID){
+        var translationFile = document.createElement('script');
+		translationFile.type = 'text/javascript';
+		translationFile.src = "locale/murodev-"+sectionID+"-"+languageID+".js";
+		document.getElementsByTagName('body')[0].appendChild(translationFile);
+
+		setTimeout(function(){
+	    		translateApp();
+		}, 2000);
+	}else{
+		  alert('No se ha creado el archivo de traduccion.');
+    }
+}
+
+function translateApp(){
+	var translateIDs = [
+		"title",
+		"description",
+		"translate"
+	];
+	
+	for(i = 0; i<translateIDs.length;i++){
+		var elementToTranslate = document.getElementsByClassName('lang__'+translateIDs[i]);
+		for(ii = 0; ii<elementToTranslate.length;ii++){
+			elementToTranslate[ii].innerHTML = translateIDs[i];
+		}
+	}
+}
